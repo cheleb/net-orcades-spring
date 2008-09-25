c1ce4e5eb301814a5c409cf6899986a780e97b60
olivier.nouguier <olivier.nouguier@5572461c-8954-11dd-90ac-37695b691a80>
2008-09-25 14:30:35 +0000

    Initial import

---
+++ orcades-spring-gwt-security/src/main/java/net/orcades/spring/gwt/security/client/GWTAuthorizationRequiredException.java
@@ -0,0 +1,10 @@
+package net.orcades.spring.gwt.security.client;
+
+public class GWTAuthorizationRequiredException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ orcades-spring-gwt-security/src/main/java/net/orcades/spring/gwt/security/server/GWTAuthenticationProcessingFilterEntryPoint.java
@@ -0,0 +1,84 @@
+package net.orcades.spring.gwt.security.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.orcades.spring.gwt.security.client.GWTAuthorizationRequiredException;
+
+import org.springframework.security.AuthenticationException;
+import org.springframework.security.ui.webapp.AuthenticationProcessingFilterEntryPoint;
+
+import com.google.gwt.user.client.rpc.SerializationException;
+import com.google.gwt.user.server.rpc.RPC;
+import com.google.gwt.user.server.rpc.RPCRequest;
+import com.google.gwt.user.server.rpc.RPCServletUtils;
+
+public class GWTAuthenticationProcessingFilterEntryPoint extends
+		AuthenticationProcessingFilterEntryPoint  {
+
+	
+	
+    private ThreadLocal<GWTPayloadHelper>  gwtPayloadHelper = new ThreadLocal<GWTPayloadHelper>();	
+   
+
+
+    
+    public void commence(ServletRequest req, ServletResponse resp,
+    		AuthenticationException authException) throws IOException,
+    		ServletException {
+
+    	
+    	
+    	HttpServletRequest request = (HttpServletRequest) req;
+    	HttpServletResponse response = (HttpServletResponse) resp;
+    	
+    	GWTPayloadHelper payloadHelper = new GWTPayloadHelper(request, response);
+    	gwtPayloadHelper.set(payloadHelper);
+    	
+    	
+    	
+    	try {
+            // Store the request & response objects in thread-local storage.
+            //
+        
+            // Read the request fully.
+            //
+            String requestPayload = RPCServletUtils.readContentAsUtf8(request);
+
+            // Let subclasses see the serialized request.
+            //
+            payloadHelper.onBeforeRequestDeserialized(requestPayload);
+    	
+
+    	
+            RPCRequest rpcRequest = RPC.decodeRequest(requestPayload, null, payloadHelper);    	
+    	
+    	try {
+			RPCServletUtils.writeResponse(request.getSession().getServletContext(), (HttpServletResponse)response, RPC.encodeResponseForFailure(null, new GWTAuthorizationRequiredException(), rpcRequest.getSerializationPolicy()), false);
+		} catch (SerializationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+    	} catch (Throwable e) {
+            // Give a subclass a chance to either handle the exception or rethrow it
+            //
+            payloadHelper.doUnexpectedFailure(e);
+          } finally {
+            // null the thread-locals to avoid holding request/response
+            //
+            gwtPayloadHelper.set(null);
+          }
+
+
+    }
+
+    
+
+   
+}
